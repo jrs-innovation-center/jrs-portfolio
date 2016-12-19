5df6b43017630c1d9a93676832932982060b4a0a
tripott <tripott@gmail.com>
2016-12-19 10:18:00 -0500

    adding pouch, rearranging state, css tweaks to footer

--- src/App.js
+++ src/App.js
@@ -8,20 +8,34 @@ const Skills = require('./components/skills')
 const Loading = require('./components/loading')
 const Footer = require('./components/footer')
 
-//const url = '/db.json'
-const url = 'http://server.pouchcloud.com/jrs-portfolio/ee739a56-6b05-487f-a4e3-6be24043d334'
+const url = '/db.json'
+//const url = 'http://server.pouchcloud.com/jrs-portfolio/ee739a56-6b05-487f-a4e3-6be24043d334'
 
 const App = React.createClass({
   getInitialState() {
     return {
-      dataLoadingState: 'Loading'
+      dataLoadingState: 'Loading',
+      profileData: null
     }
   },
 
   componentDidMount() {
     fetch(url)
       .then(res => res.json())
-      .then(data => this.setState(data))
+      .then(profileData => {
+        this.setState({dataLoadingState: 'Loaded',
+          profileData: profileData
+        })
+      })
+
+      // const db = PouchDB('foo')
+      //
+      // db.allDocs({ include_docs: true })
+      //   .then(res => res.rows.map(row => row.doc))
+      //   .then(res => {
+      //   render(JSON.stringify(res, null, 2))
+      // })
+
   },
 
   render () {
@@ -31,10 +45,10 @@ const App = React.createClass({
     return (
       <div>
           <Nav/>
-          <About info={this.state.about}/>
-          <JRS articles={this.state.articles}/>
-          <Portfolio portfolio={this.state.portfolio}/>
-          <Skills skills={this.state.skills}/>
+          <About info={this.state.profileData.about}/>
+          <JRS articles={this.state.profileData.articles}/>
+          <Portfolio portfolio={this.state.profileData.portfolio}/>
+          <Skills skills={this.state.profileData.skills}/>
           <Footer/>
       </div>
     )
